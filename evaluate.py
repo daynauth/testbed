7d05cde391b68475985fa6a9dae90af2c4ae8c67
Roland Daynauth <daynauth@gmail.com>
2021-11-17 14:15:21 -0500

    testing evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,12 @@
 import fiftyone as fo
 import fiftyone.zoo as foz
 
+if not fo.dataset_exists('efficientdet-coco'):
+    print("dataset does not exist")
+    # dataset = foz.load_zoo_dataset(
+    #     "coco-2017",
+    #     split="validation",
+    #     dataset_name="efficientdet-coco"
+    # )
 
-dataset = foz.load_zoo_dataset(
-    "coco-2017",
-    split="validation",
-    dataset_name="efficientdet-coco"
-)
-
-dataset.persistent = True
+    # dataset.persistent = True
