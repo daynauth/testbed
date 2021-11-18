9690786a8d826d1c68a21eb8b8664509d9a05835
Roland Daynauth <daynauth@gmail.com>
2021-11-18 11:52:51 -0500

    updating

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,33 @@
 import fiftyone as fo
 import fiftyone.zoo as foz
 
-if not fo.dataset_exists('efficientdet-coco'):
-    print("dataset does not exist")
-    # dataset = foz.load_zoo_dataset(
-    #     "coco-2017",
-    #     split="validation",
-    #     dataset_name="efficientdet-coco"
-    # )
+import torchvision.models as models
 
-    # dataset.persistent = True
+from torchvision.models.detection.ssdlite import SSDLiteClassificationHead
+
+def load_coco_dataset(dataset_name : str) -> fo.Dataset:
+    if not fo.dataset_exists(dataset_name):
+        dataset = foz.load_zoo_dataset(
+            "coco-2017",
+            split="validation",
+            dataset_name=dataset_name
+        )
+
+        dataset.persistent = True
+
+    else:
+        dataset = fo.load_dataset(dataset_name)
+    
+    return dataset
+
+#dataset = load_coco_dataset('efficientdet-coco')
+
+model = models.detection.ssdlite320_mobilenet_v3_large(pretrained=True)
+#model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+
+
+
+
+#print(model.head)
+
+print(model)
