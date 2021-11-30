dafaf330273b9dbeef0118d146d0e792a17a0784
Roland Daynauth <daynauth@gmail.com>
2021-11-30 11:20:14 -0500

    added torch utils

--- train.py
+++ train.py
@@ -1,49 +1,103 @@
 from random import seed
+from mongoengine.queryset import transform
 import torch
 import torchvision
 
 import utils
 from  dataset import load_coco_dataset
 from models.ssd import ssd300_resnet50
+from engine import train_one_epoch
+from coco_utils import get_coco, get_coco_kp
+
 
 import fiftyone.utils.torch as fout
 
-model = ssd300_resnet50(pretrained=False, progress=True, pretrained_backbone=True)
+def get_dataset(name, image_set, transform, data_path):
+    paths = {
+        "coco": (data_path, get_coco, 91),
+        "coco_kp": (data_path, get_coco_kp, 2)
+    }
+    p, ds_fn, num_classes = paths[name]
+
+    ds = ds_fn(p, image_set=image_set, transforms=transform)
+    return ds, num_classes
+
+
+#all the configs for training
+batch_size = 2
+lr = 0.02
+momentum = 0.9
+weight_decay = 1e-4
+lr_scheduler = "multisteplr"
+lr_steps = [16,22]
+lr_gamma = 0.1
+epochs = 4
+start_epoch = 0
+print_freq = 20
 
 device = torch.device('cuda')
 
-print('loading data')
-dataset = load_coco_dataset('ssd300_coco_train', 'train')
-num_classes = len(dataset.default_classes)
+# print('loading data')
+# dataset = load_coco_dataset('ssd300_coco_train', 'train')
+# num_classes = len(dataset.default_classes)
+
+transforms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor()
+    ]
+)
+
+dataset = torchvision.datasets.CocoDetection(
+    '/data/datasets/train/data', 
+    '/data/datasets/train/labels.json',
+    transforms=transforms
+)
 
 
 #load a small sample of the dataset for now
-view = dataset.take(500, seed = 51)
+#view = dataset.take(500, seed = 51)
+
+#print(view.first())
 
 #use the whole dataset for training in the future
-image_paths, sample_ids = zip(*[(s.filepath, s.id) for s in view])
+#image_paths, sample_ids = zip(*[(s.filepath, s.id) for s in view])
 
 #load the dataset as a pytorch dataset
-transforms = {
-    torchvision.transforms.ToTensor()
-}
 
-dataset = fout.TorchImageDataset(
-    image_paths, sample_ids=sample_ids, transform = transforms
-)
+
+# dataset = fout.TorchImageDataset(
+#     image_paths, sample_ids=sample_ids, transform = transforms
+# )
 
 print('creating data loaders')
-train_sampler = torch.utils.data.RandomSampler(dataset)
-batch_size = 2
-train_batch_sampler = torch.utils.data.BatchSampler(
-    train_sampler, batch_size, drop_last = True
-)
+# train_sampler = torch.utils.data.RandomSampler(dataset)
 
-dataloader = torch.utils.data.DataLoader(
-    dataset, batch_sampler = train_batch_sampler, collate_fn = utils.collate_fn
-)
+# train_batch_sampler = torch.utils.data.BatchSampler(
+#     train_sampler, batch_size, drop_last = True
+# )
 
-#num_classes = 91
-#print(dataset.default_classes)
-#print(len(dataset.default_classes))
-#print(len(dataset))
+# data_loader = torch.utils.data.DataLoader(
+#     dataset, batch_sampler = train_batch_sampler, collate_fn = utils.collate_fn
+# )
+
+
+
+# print('creating model')
+# model = ssd300_resnet50(
+#     pretrained=False, 
+#     progress=True, 
+#     num_classes=num_classes, 
+#     pretrained_backbone=True
+# )
+
+# model.to(device)
+# params = [p for p in model.parameters() if p.requires_grad]
+# optimizer = torch.optim.SGD(
+#         params, lr=lr, momentum=momentum, weight_decay=weight_decay)
+
+# lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=lr_steps, gamma=lr_gamma)
+
+# print("start training")
+# for epoch in range(start_epoch, epochs):
+#     train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq)
+#     lr_scheduler.step()
