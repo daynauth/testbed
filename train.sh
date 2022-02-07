25c790c0bdcfea07ccf39b27f39c4808eae46fb6
daynauth <daynauth@gmail.com>
2022-02-07 09:19:14 -0500

    added train.sh

--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@
 #    --data-augmentation retinanet\
 python train.py\
     --data-path /data/datasets/coco\
-    --batch-size 8\
+    --batch-size 16\
     --dataset coco\
     --epochs 50\
     --lr-steps 32 40\
     --aspect-ratio-group-factor 3\
     --lr 0.0002\
     --weight-decay 0.0005\
-    --model frozen_retinanet_all\
+    --model ssd300_resnet34\
